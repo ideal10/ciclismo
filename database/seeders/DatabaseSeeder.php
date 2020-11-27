810408aa084d6825d7202b0b53db10c50ece0deb
Joshua135 <josephhansdiaz30@gmail.com>
2020-11-27 08:53:37 -0500

    Joseph H. Diaz - Agregar seeder para la tabla Users.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +14,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $rootUser = new \App\Models\User;
+        $rootUser->name = 'root';
+        $rootUser->email = 'root@ciclismo.test';
+        $rootUser->email_verified_at = now();
+        $rootUser->password = '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi'; // password
+        $rootUser->remember_token = Str::random(10);
+        $rootUser->save();
+
+        \App\Models\User::factory(10)->create();
     }
 }
