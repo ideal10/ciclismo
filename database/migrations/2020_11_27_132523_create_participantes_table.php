8d8d44756b0f188cdb32dee058f183caafed52da
Joshua135 <josephhansdiaz30@gmail.com>
2020-11-27 08:30:03 -0500

    Joseph H. Diaz - Agregar migracion create_participantes_table

---
+++ database/migrations/2020_11_27_132523_create_participantes_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateParticipantesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('participantes', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre', 20);           // Nombre del participante.
+            $table->integer('identificacion');      // Identificacion personal del participante.
+            $table->year('fechaNacimiento');        // Fecha de nacimiento del participante.
+            $table->integer('edad')                 // Edad del participante.
+                ->nullable(true);
+            $table->integer('valorInscripcion');    // Valor de la inscripcion del participante.
+            $table->integer('numeroAsignado');      // Numero unico de identificacion asignado para el participante.
+            $table->integer('idCategoria')          // Categoria del participante.
+                ->nullable(true);
+            $table->timestamp('tiempoSalida', 0)    // Momento en el que salio el participante.
+                ->nullable(true);
+            $table->timestamp('tiempoLlegada', 0)   // Momento en el que llego el participante.
+                ->nullable(true);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('participantes');
+    }
+}
