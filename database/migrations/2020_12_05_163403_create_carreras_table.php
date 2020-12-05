4643b54b73d4a812ac9862de2a1223fc754ff8ad
Omar Andrés Gutierrez <75081470+OmarAGR@users.noreply.github.com>
2020-12-05 11:54:20 -0500

    migrate

---
+++ database/migrations/2020_12_05_163403_create_carreras_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCarrerasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('carreras', function (Blueprint $table) {
+            $table->id();
+            $table->integer('id_grupo');
+            $table->string('titulo');
+            $table->string('subtitulo');
+            $table->string('lugar_inicio');
+            $table->string('lugar_fin');
+            $table->integer('valor_inscripcion');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('carreras');
+    }
+}
