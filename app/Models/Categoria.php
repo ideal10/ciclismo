4580e13938b60e4f802c2bc85ca65a20c09e5632
kyokotoreno <kyokotoreno@proton.me>
2020-12-05 12:39:43 -0500

    Agregar migración de la tabla de categorias (#6) a la rama main
    * Joseph H. Diaz
    
    * Joseph H. Diaz - Agregar migracion categorias
    
    Co-authored-by: Joseph Hans Diaz <josephhansdiaz30@hotmail.com>

--- app/Models/Categoria.php
+++ app/Models/Categoria.php
@@ -8,4 +8,16 @@ use Illuminate\Database\Eloquent\Model;
 class Categoria extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'nombre',
+        'descripcion',
+        'limite_inferior',
+        'limite_superior',
+    ];
+
+    protected $hidden = [
+        'id_carrera',
+        'salida',
+    ];
 }
