05ff664d8b8b0091f656adbaa7af0d2d5ff361ab
Omar Andrés Gutierrez <75081470+OmarAGR@users.noreply.github.com>
2020-12-05 12:24:50 -0500

    Update 2020_12_05_163403_create_carreras_table.php

--- database/migrations/2020_12_05_163403_create_carreras_table.php
+++ database/migrations/2020_12_05_163403_create_carreras_table.php
@@ -16,10 +16,12 @@ class CreateCarrerasTable extends Migration
         Schema::create('carreras', function (Blueprint $table) {
             $table->id();
             $table->integer('id_grupo');
+            $table->date('fecha_carrera');
             $table->string('titulo');
             $table->string('subtitulo');
             $table->string('lugar_inicio');
             $table->string('lugar_fin');
+            $table->float('kilometros');
             $table->integer('valor_inscripcion');
             $table->timestamps();
         });
