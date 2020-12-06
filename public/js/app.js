e45101048ab391279ed2d56cc19392c727f9eade
Joseph Hans Diaz <josephhansdiaz30@gmail.com>
2020-12-06 12:00:14 -0500

    Joseph H. Diaz - Actualizar dependencias

--- public/js/app.js
+++ public/js/app.js
@@ -21304,8 +21304,9 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__("./resources/js/app.js");
-module.exports = __webpack_require__("./resources/css/app.css");
+__webpack_require__(/*! C:\Users\josep\source\repos\Ciclismo\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\josep\source\repos\Ciclismo\resources\css\app.css */"./resources/css/app.css");
+
 
 /***/ })
 
