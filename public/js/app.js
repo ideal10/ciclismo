f118223a4bf47942b5ff8d0c3984413f11b32ed1
Joseph Hans Diaz <josephhansdiaz30@hotmail.com>
2020-12-10 16:38:43 -0500

    Actualizacion de librerias

--- public/js/app.js
+++ public/js/app.js
@@ -21304,8 +21304,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\josep\source\repos\Ciclismo\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\josep\source\repos\Ciclismo\resources\css\app.css */"./resources/css/app.css");
+__webpack_require__(/*! C:\Users\josep\source\repos\Ciclismo\Project\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\josep\source\repos\Ciclismo\Project\resources\css\app.css */"./resources/css/app.css");
 
 
 /***/ })
