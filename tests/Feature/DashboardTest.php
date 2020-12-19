cc9699c4442b3976475507d031c693c49ddb3a26
Joseph Hans Diaz Gonzalez <josephhansdiaz30@gmail.com>
2020-12-19 16:01:44 -0500

    Documenting the DashboardTest class.

--- tests/Feature/DashboardTest.php
+++ tests/Feature/DashboardTest.php
@@ -9,36 +9,50 @@ use Tests\TestCase;
 class DashboardTest extends TestCase
 {
     /**
-     * Test the header of the dashboard as a random user.
+     * Test the welcome message of the dashboard as a random user.
      *
      * @return void
      */
     public function testHeaderAsRandomUser()
     {
         $user = \App\Models\User::factory()->create();
+
+        // Will assert if db connection is working correctly and the user factory is working correctly.
+        $this->assertInstanceOf(\App\Models\User::class, $user);
+
         $response = $this->actingAs($user)
                          ->get('/dashboard');
 
+        // Will assert if the '/dashboard' route is found.
         $response->assertStatus(200);
 
-        $expectedMessage = 'Bienvenido, '.$user->name;
-        $response->assertSee($expectedMessage);
+        $expectedWelcomeMessage = 'Bienvenido, '.$user->name;
+
+        // Will assert if the '/dashboard' route contains the welcome message for the random user.
+        $response->assertSee($expectedWelcomeMessage);
     }
 
     /**
-     * Test the header of the dashboard as the root user.
+     * Test the welcome message of the dashboard as the root user.
      *
      * @return void
      */
     public function testHeaderAsRoot()
     {
         $root = \App\Models\User::where('name', 'root')->first();
+
+        // Will assert if db connection is working correctly and the root user is found.
+        $this->assertInstanceOf(\App\Models\User::class, $root);
+
         $response = $this->actingAs($root)
                          ->get('/dashboard');
 
+        // Will assert if the '/dashboard' route is found.
         $response->assertStatus(200);
 
-        $expectedMessage = 'Bienvenido, root';
-        $response->assertSee($expectedMessage);
+        $expectedWelcomeMessage = 'Bienvenido, root';
+
+        // Will assert if the '/dashboard' route contains the welcome message for the root user.
+        $response->assertSee($expectedWelcomeMessage);
     }
 }
