77b7ab85512e3020906b8723927d9ddf646ac139
Joseph Hans Diaz Gonzalez <josephhansdiaz30@gmail.com>
2020-12-20 21:30:15 -0500

    Added custom colors to tailwind, and Enabled dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-    darkMode: 'media',
+    darkMode: 'class',
 
     purge: [
         './vendor/laravel/jetstream/**/*.blade.php',
@@ -14,12 +14,26 @@ module.exports = {
             fontFamily: {
                 sans: ['Nunito', ...defaultTheme.fontFamily.sans],
             },
+            colors: {
+                black: {
+                    200: '#222',
+                    300: '#333',
+                    400: '#444',
+                },
+                legacyblue: {
+                    50: '#EFF',     // Table.element1
+                    100: '#40b3ff', // Table.element1.hover
+                    450: '#9DF',    // Table.header
+                    600: '#99BBCC', // Background
+                    800: '#26ABD7', // Navmenu
+                    900: '#39C',    // Header
+                },
+                legacygreen: '#239A55',
+            },
         },
     },
 
     variants: {
         opacity: ['responsive', 'hover', 'focus', 'disabled'],
     },
-
-    plugins: [require('@tailwindcss/ui')],
 };
